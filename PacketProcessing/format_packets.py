5af4cf00d5dcf734fc3ec5acd09463e7dd9432bd
Scott <scowty.sk@outlook.com>
2024-06-11 15:56:19 +0100

    add packets

--- PacketProcessing/format_packets.py
+++ PacketProcessing/format_packets.py
@@ -1,11 +1,17 @@
-# I want to format my packets into a dataframe like format
+from ProtocolProcessing.transport_protocol import ip_to_encode_format
+
 
 def format_packets(packet_data: dict):
     print("******************************************")
     print(packet_data.keys())
     print(packet_data['layers'].keys())
     if 'ip' in packet_data['layers'].keys():
-        print("protocol:   ", packet_data['layers']['ip']['proto'])
+        print("protocol: ", packet_data['layers']['ip']['proto'])
+        print(ip_to_encode_format(int(packet_data['layers']['ip']['proto'])))
+        print("source ip:   ", packet_data['layers']['ip']['src'])
+        print("source ip:   ", packet_data['layers']['ip']['addr'])
+        print("source ip:   ", packet_data['layers']['ip']['src_host'])
+        # print("destination ip:   ", packet_data['layers']['ip']['dst'])
     print("eth keys: ", packet_data['layers']['eth'].keys())
     if 'ip' in packet_data['layers'].keys():
         print("ip data: ", packet_data['layers']['ip'].keys())
