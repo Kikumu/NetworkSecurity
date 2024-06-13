d58c0e6417beb3e0686732c9a5f9af18d0bb85eb
Scott <scowty.sk@outlook.com>
2024-06-13 11:53:53 +0100

    start on processing data link layer protocols

--- PacketProcessing/format_packets.py
+++ PacketProcessing/format_packets.py
@@ -1,24 +1,20 @@
 from ProtocolProcessing.transport_protocol import ip_to_encode_format
+from ProtocolProcessing.other_protocols import port_to_encode_format
 
 
 def format_packets(packet_data: dict):
     print("******************************************")
     print(packet_data.keys())
     print(packet_data['layers'].keys())
+    # protocols within the ip layer
     if 'ip' in packet_data['layers'].keys():
         print("protocol: ", packet_data['layers']['ip']['proto'])
         print(ip_to_encode_format(int(packet_data['layers']['ip']['proto'])))
-        print("source ip:   ", packet_data['layers']['ip']['src'])
-        print("source ip:   ", packet_data['layers']['ip']['addr'])
-        print("source ip:   ", packet_data['layers']['ip']['src_host'])
-        # print("destination ip:   ", packet_data['layers']['ip']['dst'])
-    print("eth keys: ", packet_data['layers']['eth'].keys())
-    if 'ip' in packet_data['layers'].keys():
-        print("ip data: ", packet_data['layers']['ip'].keys())
     if 'tcp' in packet_data['layers'].keys():
         print("tcp data: ", packet_data['layers']['tcp'].keys())
         print("source port: ", packet_data['layers']['tcp']['srcport'])
+        print(port_to_encode_format(int(packet_data['layers']['tcp']['srcport'])))
         print("destination port: ", packet_data['layers']['tcp']['dstport'])
+        print(port_to_encode_format(int(packet_data['layers']['tcp']['dstport'])))
         print("port: ", packet_data['layers']['tcp']['port'])
-    # print(type(packet))
     print("--------------------------------------")
